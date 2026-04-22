d5b93a8f635e2a6a933240257edc0902aa4acfa2
shahin325-admin <shahin.safari1369@gmail.com>
2026-04-22 16:09:47 +0300

    update user controller

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -1,10 +1,13 @@
 package com.codewithmosh.store.controllers;
 
+import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -13,17 +16,20 @@ public class UserController {
     private UserRepository userRepository;
 
     @RequestMapping
-    public Iterable<User> getAllUsers () {
-        return userRepository.findAll();
+    public List<UserDto> getAllUsers () {
+        return userRepository.findAll()
+                .stream()
+                .map(user -> new UserDto(user.getId(), user.getName(), user.getEmail()))
+                .toList();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
         var user = userRepository.findById(id).orElse(null);
         if (user == null) {
             return ResponseEntity.notFound().build();
         } else {
-            return ResponseEntity.ok(user);
+            return ResponseEntity.ok(new UserDto(user.getId(), user.getName(), user.getEmail()));
         }
     }
 }
