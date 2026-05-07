bf83a6350f953e80e95f326fb5edf2a35e9dcb30
shahin325-admin <shahin.safari1369@gmail.com>
2026-05-07 13:19:49 +0300

    added error handler to controller

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -7,14 +7,18 @@ import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.mappers.UserMapper;
 import com.codewithmosh.store.repositories.UserRepository;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @RestController
@@ -50,7 +54,7 @@ public class UserController {
     @PostMapping
     public ResponseEntity<UserDto> createUser(
             UriComponentsBuilder  uriComponentsBuilder,
-            @RequestBody RegisterUserRequest request) {
+            @Valid @RequestBody RegisterUserRequest request) {
 
         // convert request body to user
         User user = userMapper.toEntity(request);
@@ -107,5 +111,15 @@ public class UserController {
         userRepository.save(user);
         return ResponseEntity.noContent().build();
 
+    }
+
+
+    @ExceptionHandler (MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        var errors = new HashMap<String,String>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errors);
     }
 }
--- src/main/java/com/codewithmosh/store/dtos/RegisterUserRequest.java
+++ src/main/java/com/codewithmosh/store/dtos/RegisterUserRequest.java
@@ -1,10 +1,21 @@
 package com.codewithmosh.store.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterUserRequest {
+    @NotBlank (message = "Name is required")
+    @Size (max = 255, message = "Name must be less than 255 chars long")
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min =  8, max = 25, message = "Password must be between 8 and 25 characters long.")
     private String password;
 }
