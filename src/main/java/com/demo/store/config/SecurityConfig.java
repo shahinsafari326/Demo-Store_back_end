ff7db0195e72f142ea20ee79c151670026da7651
shahin325-admin <shahin.safari1369@gmail.com>
2026-05-10 21:00:29 +0300

    add securityFilterChain

---
+++ src/main/java/com/demo/store/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.demo.store.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // stateless session, good for token base auth
+        // disable CSRF protection as these attacks not happening with REST
+        // Authorize different part of application differently
+        http.sessionManagement(c ->
+                c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(c -> c
+                        .requestMatchers("/carts/**").permitAll() // only allow carts to be public
+                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        .anyRequest().authenticated() );
+        return http.build();
+    }
+}
